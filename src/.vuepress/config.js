65f98c5d62a9e9e97d4a335ae64a1e7c7d9e0019
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2023-01-05 20:42:26 +0900

    .

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -14,17 +14,25 @@ const sidebar = [
 ]
 
 module.exports = {
+  base: '/', // base URL
+  locales: {
+    '/': {lang: 'ja'},
+  },
   title: title,
   description: description,
   head: [
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['meta', { name: 'keywords', content: 'vuepress, netlify' }]
   ],
   themeConfig: {
     search: false,
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+    ],
     sidebar: sidebar,
   },
   plugins: [],
 }
-
